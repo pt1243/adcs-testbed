179ccdb7522798c9632f7b348377f502f4b6f832
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2025-02-19 13:45:04 +0100

    today's work on the primary pico

--- pico_primary/main.py
+++ pico_primary/main.py
@@ -1,8 +1,10 @@
 from micropython import const
 from machine import ADC, I2C, Pin, PWM, UART
-from time import sleep, sleep_ms
+from time import sleep, sleep_ms, ticks_ms, ticks_us, ticks_diff
+from collections import deque
 
 led = Pin("LED", Pin.OUT)
+led.value(0)
 
 
 # this is not technically the correct type annotation - it should be typing.NoReturn, but installing type stubs is
@@ -29,6 +31,8 @@ pwm = PWM(Pin(16, Pin.OUT, pull=None), freq=50)
 
 def set_speed(speed: float) -> None:
     speed = min(1, max(0, speed))  # clip to [0, 1]
+    MAX_SAFE_THROTTLE = 0.40  # just in case
+    speed = min(speed, MAX_SAFE_THROTTLE)
     pwm.duty_u16(65535 - int(3260 * (1 - speed) + 6540 * speed))
 
 
@@ -58,11 +62,41 @@ def setup_IMU() -> None:
 
     # set sensitivity values
     # TODO: document all these values!
+
+    DLPF_CFG = 6  # 0 through 7
+    print((DLPF_CFG << 0) == 0b0_0_000_110)
+
+    FCHOICE_B = 0  # 0 through 3
+    FS_SEL = 0  # 0 through 3
+    print((FS_SEL << 3 + FCHOICE_B << 0) == 0b000_00_0_00)
+
+    ACCEL_FS_SEL = 0  # 0 through 3
+    print((ACCEL_FS_SEL << 3) == 0b000_00_000)
+
+    A_DLPF_CFG = 0  # 0 through 7
+    ACCEL_FCHOICE_B = 1  # 0 or 1
+    DEC2_CFG = 3  # 0 through 3
+    print((A_DLPF_CFG << 0 + ACCEL_FCHOICE_B << 3 + DEC2_CFG << 4) == 0b00_11_1_000)
+
+    G_AVGCFG = 0  # 0 through 7
+    GYRO_CYCLE = 1  # 0 or 1
+    print((G_AVGCFG << 4 + GYRO_CYCLE << 7) == 0b1_000_0000)
+
+    # whatever configuration this is seems to work
     i2c.writeto_mem(IMU, REG_CONFIG, bytes([0b0_0_000_110]))
-    i2c.writeto_mem(IMU, REG_GYRO_CONFIG, bytes([0b000_00_000]))
+    #                                                 +++----------- DLPF_CFG
+    i2c.writeto_mem(IMU, REG_GYRO_CONFIG, bytes([0b000_00_0_00]))
+    #                                                  ||   ++------ FCHOICE_B
+    #                                                  ++----------- FS_SEL
     i2c.writeto_mem(IMU, REG_ACCEL_CONFIG, bytes([0b000_00_000]))
+    #                                                   ++---------- ACCEL_FS_SEL
     i2c.writeto_mem(IMU, REG_ACCEL_CONFIG_2, bytes([0b00_11_1_000]))
-    i2c.writeto_mem(IMU, REG_LP_MODE_CFG, bytes([0b1_111_0000]))
+    #                                                    || | +++--- A_DLPF_CFG
+    #                                                    || +------- ACCEL_FCHOICE_B
+    #                                                    ++--------- DEC2_CFG
+    i2c.writeto_mem(IMU, REG_LP_MODE_CFG, bytes([0b1_000_0000]))
+    #                                              | +++------------ G_AVGCFG
+    #                                              +---------------- GYRO_CYCLE
 
 
 setup_IMU()
@@ -72,14 +106,14 @@ GYRO_FACTOR = 250 / 32768  # LSB to degrees per second
 ACCEL_FACTOR = 2000 / 32768  # LSB to milli-g
 
 
-def read_16_bit_signed(value: bytes) -> int:
-    number = 256 * value[0] + value[1]
-    if number >= 32768:
-        return number - 65536
-    return number
-
-
 def read_imu() -> tuple[float, float, float, float, float, float]:
+    # no apparent performance impact from this; maybe micropython is optimizing this away?
+    def read_16_bit_signed(value: bytes) -> int:
+        number = 256 * value[0] + value[1]
+        if number >= 32768:
+            return number - 65536
+        return number
+
     # Registers needed for reading the IMU values
     REG_ACCEL_XOUT = const(0x3B)
     REG_ACCEL_YOUT = const(0x3D)
@@ -88,6 +122,7 @@ def read_imu() -> tuple[float, float, float, float, float, float]:
     REG_GYRO_YOUT = const(0x45)
     REG_GYRO_ZOUT = const(0x47)
 
+    # TODO: remove unnecessary reads (eg. accelerometer) to speed things up
     x_accel = i2c.readfrom_mem(IMU, REG_ACCEL_XOUT, 2)
     y_accel = i2c.readfrom_mem(IMU, REG_ACCEL_YOUT, 2)
     z_accel = i2c.readfrom_mem(IMU, REG_ACCEL_ZOUT, 2)
@@ -106,5 +141,55 @@ def read_imu() -> tuple[float, float, float, float, float, float]:
     )
 
 
+BUFFER_LENGTH = 100
+x_gyro_buffer = deque([0 for _ in range(BUFFER_LENGTH)], BUFFER_LENGTH)
+y_gyro_buffer = deque([0 for _ in range(BUFFER_LENGTH)], BUFFER_LENGTH)
+z_gyro_buffer = deque([0 for _ in range(BUFFER_LENGTH)], BUFFER_LENGTH)
+
+last_reported_measurement = ticks_us()
+readings = 0
 while True:
-    sleep_ms(1000)
+    # start = ticks_us()
+    imu_value = read_imu()
+    x_gyro_buffer.append(imu_value[3])
+    y_gyro_buffer.append(imu_value[4])
+    z_gyro_buffer.append(imu_value[5])
+    readings += 1
+    # end = ticks_us()
+    # print(f"Reading values into buffer took {ticks_diff(end, start) / 1000} ms")
+
+    current_time = ticks_us()
+    time_since_last_report = ticks_diff(current_time, last_reported_measurement) / 1000
+    # print(f"Time since last report: {time_since_last_report} ms")
+
+    if time_since_last_report >= 100:  # 100 ms
+        print(
+            f"Current buffer values, from {readings} readings: {sum(x_gyro_buffer) / BUFFER_LENGTH}, {sum(y_gyro_buffer) / BUFFER_LENGTH}, {sum(z_gyro_buffer) / BUFFER_LENGTH}"
+        )
+        last_reported_measurement = ticks_us()
+        readings = 0
+
+    # end = ticks_us()
+    # print(f"IMU value: {imu_value[3:]}")
+    # print(f"Time to read IMU: {ticks_diff(end, start) / 1000} ms")
+
+    # start = ticks_ms()
+    # for _ in range(1000):
+    #     read_imu()
+    # end = ticks_ms()
+    # print(f"Time in ms to read IMU, averaged over 1000 readings: {ticks_diff(end, start) / 1000}")
+    # light_sensor_0_value = light_sensor_0.read_u16()
+    # light_sensor_1_value = light_sensor_1.read_u16()
+    # light_sensor_2_value = light_sensor_2.read_u16()
+
+    # print(f"Light sensor 0: {light_sensor_0_value}")
+    # print(f"Light sensor 1: {light_sensor_1_value}")
+    # print(f"Light sensor 2: {light_sensor_2_value}")
+    # print()
+
+    # sleep_ms(200)
+
+# Notes on PID:
+# integral: just add dt * Ki * current system value to an integral term (limit this to prevent integral windup)
+# derivative: unsure if value is the last control input or the system value, but it is Kd * (current - last) / dt
+# (note: may need a minus sign; in general, check all signs with gyro +ve direction, RW direction, etc).
